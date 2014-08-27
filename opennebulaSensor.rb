09df53b7fa5f66632dc035e64111b98a3b42dd2e
Andrea Guarise <andrea.guarise@to.infn.it>
2014-08-27 11:28:23 +0200

    Added disk size to ssm record

--- opennebulaSensor.rb
+++ opennebulaSensor.rb
@@ -70,7 +70,7 @@ class OneRecordSSM < LocalRecord
     "NetworkInbound: " + record['networkInbound'] + "\n" +
     "NetworkOutbound: " + record['networkOutbound'] + "\n" +
     "Memory: " + record['memory'] + "\n" +
-    "Disk: " + "" + "\n" +
+    "Disk: " + record['diskSize'] + "\n" +
     "StorageRecordId: " + "" + "\n" +
     "ImageId: " + record['diskImage'] + "\n" +
     "CloudType: " + "OpenNebula" + "\n" + "%%\n"
